c56b03d2fd13d25a9b6155d52540593d59a882e2
Denisa Kita <denisakita7@gmail.com>
2023-12-30 15:02:00 +0100

    change css of Header.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import {AddTask, Footer, Header, ShowTask} from "./components";
+
 
 function App() {
     return (
         <div className="App">
-
+            <Header/>
+            <AddTask/>
+            <ShowTask/>
+            <Footer/>
         </div>
     );
 }
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { Header } from './Header';
+export { AddTask } from './AddTask';
+export { ShowTask } from './ShowTask';
+export { Footer } from './Footer';
