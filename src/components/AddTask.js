5a41dac46499c6aa804b3c951371acc73bce9f62
Denisa Kita <denisakita7@gmail.com>
2023-12-30 15:16:29 +0100

    working with add task and show task

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,3 +1,11 @@
 export const AddTask = () => {
-  
+  return(
+     <section className="addTask">
+         <form>
+             <input type="text" name="task" autoComplete="off"
+                    placeholder="Task Name" maxLength="25"/>
+             <button type="submit">Add</button>
+         </form>
+     </section>
+  )
 }
--- src/components/ShowTask.js
+++ src/components/ShowTask.js
@@ -1,3 +1,33 @@
 export const ShowTask = () => {
-  
+    const tasks = [
+        {id:1000,name:"Task 1", time:"1234"},
+        {id:2000,name:"Task 2", time:"2222"},
+        {id:3000,name:"Task 3", time:"4444"},
+    ]
+
+    return (
+        <section className="showTask">
+            <div className="head">
+                <div>
+                    <span className="title">Todo</span>
+                    <span className="count">0</span>
+                </div>
+                <button className="clearAll">Clear All</button>
+            </div>
+            <ul>
+                {tasks.map((task)=>(
+                    <li>
+                        <p>
+                            <span className="name">{task.name}</span>
+                            <span className="time">{task.time}</span>
+                        </p>
+                        <i className="bi bi-pencil-square"></i>
+                        <i className="bi bi-trash"></i>
+                    </li>
+                ))}
+
+
+            </ul>
+        </section>
+    )
 }
